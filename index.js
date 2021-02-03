05611740446ba50205a8fd46180b6b2297eecf73
itsmeandi <78503174+itsmeandi@users.noreply.github.com>
2021-02-04 04:01:16 +0800

    index.js

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ const menu = require("./lib/menu.js")
 
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
-const BotName = 'ARIWF BOT V1'; 
-const instagram = '@arieff_westa'; 
+const BotName = 'Sherlock Bot'; 
+const instagram = '@andibasoamirullah_'; 
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Arief Westa\n' // Nama kamu
-            + 'ORG:Arief Bot;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=62895600555028:+62 895-6005-55028\n' //Nomor whatsapp kamu
+            + 'FN:SherlockHolmes\n' // Nama kamu
+            + 'ORG:Sherlock Bot;\n' // Nama bot
+            + 'TEL;type=CELL;type=VOICE;waid=6282292119677 +62 822-9211-9677\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
 {
@@ -1451,6 +1451,6 @@ Silakan pilih :
 
 }
 //Hay Bro 
-//create @arieff_westa don't delate this please
+//create @andibasoamirullah_ don't delate this please
 	
 })
